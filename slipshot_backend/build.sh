39bb8f315e232f539ca1b141485b664f54cb923e
pawit5001 <weerakoop2008@hotmail.com>
2026-02-03 06:34:27 +0700

    feat: auto create superuser from env vars

--- slipshot_backend/build.sh
+++ slipshot_backend/build.sh
@@ -6,3 +6,8 @@ pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
 python manage.py migrate
+
+# Create superuser if env vars are set
+if [ -n "$DJANGO_SUPERUSER_USERNAME" ] && [ -n "$DJANGO_SUPERUSER_EMAIL" ] && [ -n "$DJANGO_SUPERUSER_PASSWORD" ]; then
+    python manage.py createsuperuser --noinput || true
+fi
